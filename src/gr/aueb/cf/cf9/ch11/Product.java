d50345b5976d524ed8257b8e91280c61f840214d
pantsath <pants.ath@gmail.com>
2025-12-03 20:45:46 +0200

    feat: add product file

---
+++ src/gr/aueb/cf/cf9/ch11/Product.java
@@ -0,0 +1,61 @@
+package gr.aueb.cf.cf9.ch11;
+
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean stock;
+
+    public Product() {
+
+    }
+
+    public Product(long id, String name, String description, double price, boolean stock) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isStock() {
+        return stock;
+    }
+
+    public void setStock(boolean stock) {
+        this.stock = stock;
+    }
+}
